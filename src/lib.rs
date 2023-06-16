5204f314272747ac20da1fb4f9072a14f5d3ea86
Aaron Goodman <aarongoodm@gmail.com>
2023-06-16 04:03:51 -0400

    get users

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod database;
+pub mod handlers;
+pub mod models;
+pub mod schema;
+pub mod utils;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+use reddit_clone::{database::get_connection_pool, handlers::list_users};
+
+#[tokio::main]
+async fn main() {
+    let connection_pool = get_connection_pool().await;
+    let app = Router::new()
+        .route("/users", get(list_users))
+        .with_state(connection_pool);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
